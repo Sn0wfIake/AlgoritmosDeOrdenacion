f60bfb3779bc416c8fd3fe03e30bc0e32b8fa395
alex <alexmatemese@gmail.com>
2020-11-11 18:35:43 +0100

    asd

---
+++ Algoritmos/src/Hilos/HiloBurbuja.java
@@ -0,0 +1,42 @@
+package Hilos;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class HiloBurbuja extends Thread {
+
+	JPanel panel;
+	int array[];
+	JButton botones[];
+
+	public HiloBurbuja(JPanel panel, int[] array) {
+		this.panel = panel;
+		this.array = array;
+	}
+
+	public void run() {
+		int i, j, aux;
+			for (i = 0; i < array.length - 1; i++) {
+				for (j = 0; j < array.length - i - 1; j++) {
+					if (array[j + 1] < array[j]) {
+						aux = array[j + 1];
+						array[j + 1] = array[j];
+						((JButton) this.panel.getComponent(j + 1)).setText("" + array[j]);
+						array[j] = aux;
+						((JButton) this.panel.getComponent(j)).setText("" + aux);
+
+					}
+					try {
+						sleep(100);
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					this.panel.getComponent(j).setBackground(Color.blue);
+				}
+				this.panel.getComponent(19).setBackground(Color.blue);
+			}
+		}
+}
